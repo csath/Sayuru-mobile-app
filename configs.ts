b8ce7bf1ea8bb1dbf9f64ebb7498de94e4b8917c
Chanaka Athurugiriya <chanakaathurugiriya@gmail.com>
2022-05-25 20:27:50 +0530

    Update configs.ts

--- configs.ts
+++ configs.ts
@@ -1,8 +1,8 @@
 export const BASE_URL = 'https://sayuruapi.herokuapp.com';
 export const API_KEY = 'ae3d9d0127aa42d493ff7eecf46b2c75';
-export const MapBox_Token = "sk.eyJ1IjoiY2hhcml0aHciLCJhIjoiY2trMHdpcmhpMGxzejJucGN6aDJsZHZkYyJ9.dF32madDtpb9v8l4tpqYEw";
+export const MapBox_Token = "sk.eyJ1IjoiY2hhcml0aHciLCJhIjoiY2trMHdpcmhpMGxzejJucGN6aDJsZHZkYyJ9.dF32madDtpb9v8l4tpqYEw--";
 
-export const PRIVACY_POLICY_LINK = 'https://dialog.lk';
-export const TERMS_AND_CONDITIONS_LINK = 'https://dialog.lk';
+export const PRIVACY_POLICY_LINK = '----';
+export const TERMS_AND_CONDITIONS_LINK = '----';
 
 export const SUBSCRIPTION_SKU = 'com.sayuru';
